5f352e7d8acbeb73f7bc468744841093db0c4458
Mohammed Saif <mohammedsaif2803@gmail.com>
2026-06-19 11:11:15 +0530

    Add files via upload

---
+++ xml parse local.py
@@ -0,0 +1,13 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('books.xml')
+root = tree.getroot()
+
+for book in root.findall('book'):
+    title  = book.find('title').text
+    author = book.find('author').text
+    year   = book.find('year').text
+    print(f"Title : {title}")
+    print(f"Author: {author}")
+    print(f"Year  : {year}")
+    print()
